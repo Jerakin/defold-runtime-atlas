18dd52f67c5ba2d7dde643dfbc55d493be9f8c5d
Jerakin <the_rensvind@hotmail.com>
2023-02-22 22:59:31 +0100

    wip

---
+++ dynamic-atlas/dynamic-atlas.lua
@@ -0,0 +1,152 @@
+local rowpacking = require "dynamic-atlas/row-packing"
+
+local M  = {}
+
+M.algorithm = rowpacking.pack
+
+local function convert_string_buffer_to_buffer(blob)
+	-- string with binary pixel data in rgba format
+	local size = #blob / 4
+
+	-- create rgba buffer and get the stream where the pixcels should be stored
+	local new_buffer = buffer.create( size, { {name=hash("rgba"), type=buffer.VALUE_TYPE_UINT8, count=4 } })
+	local stream = buffer.get_stream(new_buffer, hash("rgba"))
+
+	-- copy pixels to stream in buffer
+	local i = 1
+	while i < #blob do
+		stream[i] = string.byte(blob, i) -- r
+		i = i + 1
+
+		stream[i] = string.byte(blob, i) -- g
+		i = i + 1
+
+		stream[i] = string.byte(blob, i) -- b
+		i = i + 1
+
+		stream[i] = string.byte(blob, i) -- a
+		i = i + 1
+	end
+	return new_buffer
+end
+
+local function flip_texture_vertically(buffer_string, outw, outh, bytes_per_pixel)
+	local pixels = {}
+	buffer_string:gsub(".",function(c) table.insert(pixels,c) end)
+	local byte1
+	local byte2
+	local offset1
+	local offset2
+	for yi=1, outh / 2 do
+		for xi=1, outw do 
+			offset1 = (xi + (yi * outw)) * bytes_per_pixel;
+			offset2 = (xi + ((outh - 1 - yi) * outw)) * bytes_per_pixel;
+			for bi=0, bytes_per_pixel do
+				byte1 = pixels[offset1 + bi];
+				byte2 = pixels[offset2 + bi];
+				pixels[offset1 + bi] = byte2;
+				pixels[offset2 + bi] = byte1;
+			end
+		end
+	end
+	return table.concat(pixels, "")
+end
+
+local function split(inputstr, sep)
+	if sep == nil then
+		sep = "%s"
+	end
+	local t={}
+	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+		table.insert(t, str)
+	end
+	return t
+end
+
+
+--This function finds the filename when given a complete path 
+function get_filename(path)
+	local parts = split(path, "/")
+	return parts[#parts]
+end
+
+
+local function get_package_data(list_of_textures)
+	local pack_data = {}
+	for i in pairs(list_of_textures or {}) do
+		local image_path = list_of_textures[i]
+		local file = io.open(image_path, "rb")
+		if file then
+			local buffer = file:read("*all")
+			file:close()
+			local img = image.load(buffer)
+			local buffer = flip_texture_vertically(img.buffer, img.width, img.height, 4)
+			table.insert(pack_data, {name=get_filename(image_path), buffer=convert_string_buffer_to_buffer(buffer), w=img.width, h=img.height})
+		end
+	end
+	return pack_data
+end
+
+
+local function get_atlas_parameters(image, atlas_params)
+	table.insert(atlas_params.animations, {
+		id          = image.name,
+		width       = image.w,
+		height      = image.h,
+		frame_start = 1,
+		frame_end   = 2,
+	})
+	table.insert(atlas_params.geometries, {
+		vertices  = {
+			image.x,   image.y,
+			image.x,   image.y + image.h,
+			image.x + image.w, image.y + image.h,
+			image.x + image.w, image.y
+		},
+		uvs = {
+			image.x,   image.y,
+			image.x,   image.y + image.h,
+			image.x + image.w, image.y + image.h,
+			image.x + image.w, image.y
+		},
+		indices = {0,1,2,0,2,3}
+	})
+	return atlas_params
+end
+
+function M.pack(atlas_name_or_resource, list_of_textures, width, height)
+	local pack_data = get_package_data(list_of_textures)
+	M.algorithm(pack_data, width, height)
+	pprint(pack_data)
+	local atlas_creation_params = {
+		width  = 93,
+		height = 78,
+		type   = resource.TEXTURE_TYPE_2D,
+		format = resource.TEXTURE_FORMAT_RGBA,
+	}
+	
+	if type(atlas_name_or_resource) == "string" then
+		local atlas_name = "/dynatlas/" .. atlas_name_or_resource .. ".texture"
+		local texture_id = resource.create_texture(atlas_name.."c", atlas_creation_params)
+		local atlas_id
+		local set_params
+		local atlas_params = {animations={}, geometries={}}
+		atlas_params.texture = atlas_name.."c"
+		for i in pairs(pack_data) do
+			local img = pack_data[i]
+			
+			set_params = {width=img.w, height=img.h, x=img.x, y=img.y, type=resource.TEXTURE_TYPE_2D, format=resource.TEXTURE_FORMAT_RGBA}
+			resource.set_texture(texture_id, set_params, img.buffer)
+			
+			atlas_params = get_atlas_parameters(img, atlas_params)
+		end
+		pprint(atlas_params)
+		atlas_id = resource.create_atlas(atlas_name .. "setc", atlas_params)
+		return atlas_id
+	else
+		
+	end	
+end
+
+
+return M
---
+++ dynamic-atlas/row-packing.lua
@@ -0,0 +1,49 @@
+local M = {}
+
+
+
+local function height_sort(a, b)
+	return a.h < b.h
+end
+
+function M.pack(list_of_textures, width, height)
+	assert(width, "Provide texture width")
+	assert(height, "Provide texture height")
+	table.sort(list_of_textures, height_sort)
+
+	local xPos = 0;
+	local yPos = 0;
+	local largestHThisRow = 0;
+
+	-- Loop over all the rectangles
+	for i in pairs(list_of_textures) do
+		local rect = list_of_textures[i]
+		-- If this rectangle will go past the width of the image
+		-- Then loop around to next row, using the largest height from the previous row
+		if ((xPos + rect.w) > width) then
+			yPos = yPos + largestHThisRow;
+			xPos = 0;
+			largestHThisRow = 0;
+		end
+
+		-- If we go off the bottom edge of the image, then we've failed
+		if ((yPos + rect.h) > height) then
+			return
+		end
+		
+		-- This is the position of the rectangle
+		rect.x = xPos;
+		rect.y = yPos;
+
+		-- Move along to the next spot in the row
+		xPos = xPos + rect.w;
+
+		-- Just saving the largest height in the new row
+		if (rect.h > largestHThisRow) then
+			largestHThisRow = rect.h;
+		end
+	end
+	return list_of_textures
+end
+
+return M
